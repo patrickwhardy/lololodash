5fb05091995c7b4daeeb30ff50e3a43d78c05a97
patrickwhardy <patrickwhardy@gmail.com>
2016-06-23 12:32:43 -0600

    temps exercise

--- forEach.js
+++ forEach.js
@@ -1,44 +1,44 @@
 var _ = require("lodash");
 
-function determineSize(population){
-  if (population["population"] > 1.5 ){
-    return "big";
-  } else if (population["population"] > 0.5 ){
-    return "med";
-  } else {
-    return "small";
-  }
-};
-
+// function determineSize(population){
+//   if (population["population"] > 1.5 ){
+//     return "big";
+//   } else if (population["population"] > 0.5 ){
+//     return "med";
+//   } else {
+//     return "small";
+//   }
+// };
+//
 var population = function(h){
-  return _.reduce(h, function(result, value, key) {
-    console.log("result:")
-    console.log(result);
-    console.log("value:")
-    console.log(value)
-    console.log("key:")
-    console.log(key)
-    value["size"] = determineSize(value);
-    result[key] = value;
-    return {key: result};
-  });
+//   return _.reduce(h, function(result, value, key) {
+//     console.log("result:")
+//     console.log(result);
+//     console.log("value:")
+//     console.log(value)
+//     console.log("key:")
+//     console.log(key)
+//     value["size"] = determineSize(value);
+//     result[key] = value;
+//     return {key: result};
+//   });
 
-  // var sizes = _.map(h, function(value, key){
-  //   var size;
-  //   if (value["population"] > 1.5 ){
-  //     size = "big";
-  //   } else if (value["population"] > 0.5 ){
-  //     size = "med";
-  //   } else {
-  //     size = "small";
-  //   }
-  //   return size;
-  // });
-  // var counter = 0;
-  // return _.forEach(h, function(value, key){
-  //   value["size"] = sizes[counter]
-  //   counter++
-  // });
+  var sizes = _.map(h, function(value, key){
+    var size;
+    if (value["population"] > 1.5 ){
+      size = "big";
+    } else if (value["population"] > 0.5 ){
+      size = "med";
+    } else {
+      size = "small";
+    }
+    return size;
+  });
+  var counter = 0;
+  return _.forEach(h, function(value, key){
+    value["size"] = sizes[counter]
+    counter++
+  });
 };
 
 module.exports = population;
